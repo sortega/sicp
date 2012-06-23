20ae6e8f6bb8919189ec912967076eee63f9764a
Sebastian Ortega <sortega@tid.es>
2012-06-23 16:50:30 +0200

    Section 2.2.2

--- src/sicp/chapter2_2.clj
+++ src/sicp/chapter2_2.clj
@@ -79,3 +79,152 @@
     (f (first aseq))
     (recur f (rest aseq))))
 
+
+;; Exercise 2.24
+;; =============
+
+(defn count-leaves [x]
+  (cond (not (sequential? x)) 1
+        (empty? x) 0
+        :else (+ (count-leaves (first x))
+                 (count-leaves (rest x)))))
+
+; (count-leaves [1 [2 [3 4]]])
+; => 4
+;
+; as a tree 1 -> 2 -> 3 -> 4
+
+
+;; Exercise 2.25
+;; =============
+
+(-> [1 3 [5 7] 9] rest rest first rest first)
+(-> [[7]] first first)
+(-> '(1 (2 (3 (4 (5 (6 7)))))) 
+  rest first rest first rest first rest first rest first rest first)
+
+
+;; Exercise 2.26
+;; =============
+
+(def x (list 1 2 3))
+(def y (list 4 5 6))
+
+;=> (concat x y)
+;(1 2 3 4 5 6)
+
+;=> (cons x y)
+;((1 2 3) 4 5 6)
+
+;=> (list x y)
+;((1 2 3) (4 5 6))
+
+
+;; Exercise 2.27
+;; =============
+
+(defn deep-reverse [elem]
+  (if (sequential? elem) 
+    (loop [aseq elem
+           rev  ()]
+      (if (empty? aseq)
+        rev
+        (recur (rest aseq)
+               (cons (deep-reverse (first aseq)) rev))))
+    elem))
+
+
+;; Exercise 2.28
+;; =============
+
+(defn fringe [tree]
+  (mapcat 
+    #((if (sequential? %) fringe list) %)
+    tree))
+
+
+;; Exercise 2.29
+;; =============
+
+(defn make-mobile [left right]
+  (list left right))
+
+(defn make-branch [length structure]
+  (list length structure))
+
+(def left-branch first)
+(def right-branch second)
+(def branch-length first)
+(def branch-structure second)
+(def mobile? list?)
+
+(defn total-weight [structure]
+  (if (mobile? structure)
+    (letfn [(branch-weight [branch]
+              (total-weight (branch-structure branch)))]
+      (+ (branch-weight (left-branch  structure))
+         (branch-weight (right-branch structure))))
+    structure))
+
+(defn balanced? [structure]
+  (if (mobile? structure)
+    (let [left  (-> structure left-branch  branch-structure)
+          right (-> structure right-branch branch-structure)]
+      (and (balanced? left)
+           (balanced? right)
+           (= (total-weight left) (total-weight right))))
+    true))
+
+(comment
+  ; If we replace mobile representation as follows we need no change on the
+  ; total-weight or balanced? functions. ;)
+  ; I've used maps insted of cons since, Clojure does not allow for improper
+  ; lists (i.e. (cons 1 2)).
+
+  (defn make-mobile [left right]
+    {:left left
+     :right right})
+  (defn make-branch [length structure]
+    {:length length
+     :structure structure})
+
+  (def left-branch :left)
+  (def right-branch :right)
+  (def branch-length :length)
+  (def branch-structure :structure)
+  (def mobile? map?))
+
+
+;; Exercise 2.30
+;; =============
+
+(defn square-tree [tree]
+  (if (empty? tree)
+    ()
+    (let [sub-tree (first tree)
+          rest-tree (rest tree)]
+      (cons 
+         (if (sequential? sub-tree)
+           (square-tree sub-tree)
+           (square sub-tree))
+         (square-tree rest-tree)))))
+
+(defn square-tree [tree]
+  (map (fn [sub-tree]
+         (if (sequential? sub-tree)
+           (square-tree sub-tree)
+           (square sub-tree)))
+       tree))
+
+
+;; Exercise 2.31
+;; =============
+
+(defn tree-map [f tree]
+  (map (fn [sub-tree]
+         (if (sequential? sub-tree)
+           (tree-map f sub-tree)
+           (f sub-tree)))
+       tree))
+
+(def square-tree (partial tree-map square))
